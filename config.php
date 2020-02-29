a8092cf87a86b99ce0890e3fe6e362b0cc2561d9
paulbalan <creativezone98@gmail.com>
2020-02-29 20:19:47 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,6 @@
 <?php
-//Database
+//This is your connection to the database
+//Every field that is prefixed by "insert_" should be updated. Also, you can change the charset
 define('DB_SERVER', 'insert_host_name');
 define('DB_INFO', 'mysql:host=insert_host_name;dbname=insert_name_of_database;charset=utf8mb4');
 define('DB_USERNAME', 'insert_username');
