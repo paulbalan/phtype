eb883daa0198e3b828668fc790674e9e8d3123c7
paulbalan <creativezone98@gmail.com>
2020-02-29 20:16:32 +0200

    Add files via upload

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+//Database
+define('DB_SERVER', 'insert_host_name');
+define('DB_INFO', 'mysql:host=insert_host_name;dbname=insert_name_of_database;charset=utf8mb4');
+define('DB_USERNAME', 'insert_username');
+define('DB_PASSWORD', 'insert_password_of_user');
+define('DB_DATABASE', 'insert_name_of_database');
